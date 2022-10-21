dc32418be3b818caade413673bd564461c479719
Leomar <leomar.tariao.sanguenza@gmail.com>
2022-10-21 19:36:39 +0800

    Second commit

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -1,3 +1,5 @@
+<html><head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
 @extends('students.layout')
 @section('content')
 <div class="card">
@@ -20,3 +22,4 @@
   </div>
 </div>
 @stop
+</html>
